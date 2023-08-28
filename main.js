7eb3cb8fc145a55d9dba69e235e25f3a4d9b7bd4
Orellana Leonardo <leonardo.orellana@accussys.com>
2023-08-28 18:34:18 -0300

    se agregan campos, se agrega arrow, y la nueva tabla responsive

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function cambiarTabla() {
-    var tabla1 = document.getElementById("tabla1");
-    var tabla2 = document.getElementById("tabla2");
+    var tabla1 = document.getElementById("table1");
+    var tabla2 = document.getElementById("table2");
 
     if (tabla1.classList.contains("hidden")) {
         tabla1.classList.remove("hidden");
